337bb729620b815a117c02d3fc1d6d47f77e35d5
John Safrit <jsafrit@lmgholdings.com>
2015-02-06 13:19:31 -0500

    Fixed directory change. Fixed fread, fwrite calls.

--- main.c
+++ main.c
@@ -11,7 +11,8 @@ void next_source( const char * pattern, char * fn )
 
     if (dir == NULL)
     {
-        dir = opendir (".\\root\\");
+        //dir = opendir (".\\root\\");
+        dir = opendir (".");
         if (dir == NULL)
         {
             /* could not open directory */
@@ -40,14 +41,18 @@ int main()
 {
     const char source_root_fn[] = "S0";
     const char dest_root_fn[] = "D0";
-    const unsigned int max_copies = 9;    // Max allowed is 9999
+    const unsigned int max_copies = 99;    // Max allowed is 9999
 
     char source_fn[13];
     char dest_fn[13];
+
     FILE *src_fh, *dst_fh;
     char buffer[BUFF_SIZE];
 
     int i,j,nrbuff,nwbuff;
+
+
+    chdir(".\\root\\");
 
     next_source(source_root_fn ,source_fn);
     do
@@ -55,10 +60,11 @@ int main()
         printf("Source Filename: %s\t", source_fn);
         src_fh = fopen(source_fn, "rb");
 
-        // create destination filename
+        // create destination root filename
         strcpy(dest_fn,source_fn);
         strncpy(dest_fn, dest_root_fn, 2);
         printf("Dest Root Filename: %s\n", dest_fn);
+
         for (i=1; i<=max_copies; i++)
         {
             char counter[5];
@@ -68,16 +74,15 @@ int main()
             printf("Dest Filename: %s\n", dest_fn);
             // copy the file here
             dst_fh = fopen( dest_fn, "wb" );
-            while ( (nrbuff = fread(buffer, BUFF_SIZE, 1, src_fh), nrbuff >0 ) )
+            while ( (nrbuff = fread(buffer, 1, BUFF_SIZE, src_fh), nrbuff >0 ) )
             {
-                nwbuff = fwrite(buffer, nrbuff, 1, dst_fh);
+                nwbuff = fwrite(buffer, 1, nrbuff, dst_fh);
                 if (nwbuff != nrbuff)
                     printf("Write ERROR!\n");
             }
             fclose(dst_fh);
             rewind(src_fh);
         }
-
         next_source(source_root_fn ,source_fn);
 
     } while (strlen(source_fn) > 0);
